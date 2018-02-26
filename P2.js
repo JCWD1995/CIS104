f5a0d8036a503e3676c043781afff6ce7feeb2e8
JCWD1995 <wdole@hotmail.com>
2018-02-26 18:37:48 -0500

    Clean code

--- P2.js
+++ P2.js
@@ -43,9 +43,9 @@ main();
 function setContinueResponse() {
 	if (continueResponse === YES || continueResponse === NO) {
 		continueResponse = -1;
-		while (isNaN(continueResponse) || continueResponse > 1 || continueResponse < 0) {
+		while (isNaN(continueResponse) || (continueResponse !== YES && continueResponse !== NO)) {
 			continueResponse = Number(PROMPT.question(`\nDo you want to continue? [1=yes,0=no]\n`));
-			if (isNaN(continueResponse) || continueResponse > 1 || continueResponse < 0) {
+			if (isNaN(continueResponse) || (continueResponse !== YES && continueResponse !== NO)) {
 				console.log("INVALID INPUT");
 				PROMPT.question(`\nPress enter to continue.\n`)
 				process.stdout.write('\x1Bc');
@@ -102,7 +102,7 @@ function checkName(test) {
 			test = Number(0);
 		}
 	}
-	if (test !== Number(0)) {
+	if (test !== 0) {
 		if (2 < length && length < 15) {
 			test = Number(1);
 		} else {
@@ -118,7 +118,7 @@ function setBirth() {
 	birthMonth = setBirthMonth();
 	birthDay = setBirthDay(birthYear,birthMonth);
 	birth = birthYear + "/" + birthMonth + "/" + birthDay;
-	let confirmBirth = confirmVar("The customer was born on " + birth + ".");
+	let confirmBirth = confirmVar("The customer was born on " + birth + ". Format is yyyy/mm/dd.");
 	if (confirmBirth === YES) {
 		setAge(birthDay, birthMonth, birthYear);
 	} else {
@@ -134,14 +134,13 @@ function setBirthYear() {
 	const MAXYEAR = today.getFullYear()-MIN_AGE; //We don't want anyone under age 15
 	process.stdout.write('\x1Bc');
 	while (isNaN(year) || year < MINYEAR || year > MAXYEAR) {
-		year = PROMPT.question(`\nWhat year was ` + firstName + ` born in?\n`);
+		year = Number(PROMPT.question(`\nWhat year was ` + firstName + ` born in?\n`));
 		if (isNaN(year) || year < MINYEAR || year > MAXYEAR) {
 			console.log("INVALID INPUT");
 			PROMPT.question(`\nPress enter to continue.\n`); //Freezes screen
 			process.stdout.write('\x1Bc');
 		}
 	}
-	year = Number(year);
 	let confirmBirth = confirmVar(firstName + " was born in the year " + year + ".");
 	if (confirmBirth === 0) {
 		return setBirthYear();
@@ -156,14 +155,13 @@ function setBirthMonth() {
 	const MAXMONTH = Number(12);
 	process.stdout.write('\x1Bc');
 	while (isNaN(month) || month < MINMONTH || month > MAXMONTH) {
-		month = PROMPT.question(`\nWhat month was ` + firstName + ` born in?\nPlease enter a number between 1 and 12.\n`);
+		month = Number(PROMPT.question(`\nWhat month was ` + firstName + ` born in?\nPlease enter a number between 1 and 12.\n`));
 		if (isNaN(month) || month < MINMONTH || month > MAXMONTH) {
 			console.log("INVALID INPUT");
 			PROMPT.question(`\nPress enter to continue.\n`)
 			process.stdout.write('\x1Bc');
 		}
 	}
-	month = Number(month);
 	monthName = setMonthName(month);
 	let confirmBirth = confirmVar(firstName + " was born in the month of " + monthName + ".");
 	if (confirmBirth === 0) {
@@ -173,33 +171,31 @@ function setBirthMonth() {
 }
 
 function setMonthName(month) {
-	let name;
 	if (month === Number(1)) {
-		name = "January";
+		return "January";
 	} else if (month === Number(2)) {
-		name = "February";
+		return "February";
 	} else if (month === Number(3)) {
-		name = "March";
+		return "March";
 	} else if (month === Number(4)) {
-		name = "April";
+		return "April";
 	} else if (month === Number(5)) {
-		name = "May";
+		return "May";
 	} else if (month === Number(6)) {
-		name = "June";
+		return "June";
 	} else if (month === Number(7)) {
-		name = "July";
+		return "July";
 	} else if (month === Number(8)) {
-		name = "August";
+		return "August";
 	} else if (month === Number(9)) {
-		name = "September";
+		return "September";
 	} else if (month === Number(10)) {
-		name = "October";
+		return "October";
 	} else if (month === Number(11)) {
-		name = "November";
+		return "November";
 	} else {
-		name = "December";
+		return "December";
 	}
-	return name;
 }
 
 function setBirthDay(year,month) {
@@ -210,14 +206,13 @@ function setBirthDay(year,month) {
 	const MAXDAY = setMaxDay(year,month);
 	process.stdout.write('\x1Bc');
 	while (isNaN(day) || day < MINDAY || day > MAXDAY) {
-		day = PROMPT.question(`\nWhat day was ` + firstName + ` born on in ` + monthName + `?`);
+		day = Number(PROMPT.question(`\nWhat day was ` + firstName + ` born on in ` + monthName + `?`));
 		if (isNaN(day) || day < MINDAY || day > MAXDAY) {
 			console.log("INVALID INPUT");
 			PROMPT.question(`\nPress enter to continue.`)
 			process.stdout.write('\x1Bc');
 		}
 	}
-	day = Number(day);
 	confirmBirth = confirmVar(firstName + " was born on " + day + " in " + monthName + ".");
 	if (confirmBirth === 0) {
 		return setBirthDay();
@@ -226,19 +221,17 @@ function setBirthDay(year,month) {
 }
 
 function setMaxDay(year,month) {
-	let maxDay;
 	if (month === Number(1) || month === Number(3) || month === Number(5) || month === Number(7) || month === Number(8) || month === Number(10) || month === Number(12)) {
-		maxDay = Number(31);
+		return Number(31);
 	} else if (month === Number(4) || month === Number(6) || month === Number(9) || month === Number(11)) {
-		maxDay = Number(30);
+		return Number(30);
 	} else if (month === Number(2)) {
 		if (year%4 !== Number(0) || year === Number(1900)) {
-			maxDay = Number(28);
+			return Number(28);
 		} else {
-			maxDay = Number(29);
+			return Number(29);
 		}
 	}
-	return maxDay;
 }
 
 function setAge(birthDay, birthMonth, birthYear) {
@@ -246,7 +239,7 @@ function setAge(birthDay, birthMonth, birthYear) {
 	let day = today.getDate();
 	let month = today.getMonth()+1;
 	let year = today.getFullYear();
-	let age = year - birthYear;
+	let age = Number(year - birthYear);
 	if (birthMonth > month) {
 		age = age-1;
 	} else if (birthMonth === month) {
@@ -255,9 +248,9 @@ function setAge(birthDay, birthMonth, birthYear) {
 		} else if (birthDay === day) {
 			process.stdout.write('\x1Bc');
 			console.log("Happy Birthday " + firstName + " " + lastName + "!");
+			PROMPT.question("Press enter to continue.");
 		}
 	}
-	age = Number(age);
 	setAddition(age);
 }
 
@@ -274,10 +267,11 @@ function setAddition(age) {
 function setFault() {
 	let confirmFault;
 	let atFault = -1;
+	const NO_FAULT = 0;
 	process.stdout.write('\x1Bc');
-	while (isNaN(atFault) || atFault < 0) {
+	while (isNaN(atFault) || atFault < NO_FAULT) {
 		atFault = PROMPT.question(`\nHow many at fault accidents has ` + firstName + ` had in the past three years?`);
-		if (isNaN(atFault) || atFault < 0) {
+		if (isNaN(atFault) || atFault < NO_FAULT) {
 			console.log("INVALID INPUT");
 			PROMPT.question(`\nPress enter to continue.`)
 			process.stdout.write('\x1Bc');
@@ -303,19 +297,19 @@ function setDueDate() {
 	let confirmDate;
 	let today = new Date();
 	dueDate = today.getDate();
+	process.stdout.write('\x1Bc');
 	if (dueDate > 28) {
 		dueDate = 28;
 	}
-	while (isNaN(confirmDate) || confirmDate > 1 || confirmDate < 0) {
+	while (isNaN(confirmDate) || (confirmDate === YES && confirmDate === NO)) {
 		console.log(firstName + " has been given the due date of " + dueDate + ".");
-		confirmDate = PROMPT.question(`\nIs this alright? [1=yes,0=no]`);
-		if (isNaN(confirmDate) || confirmDate > 1 || confirmDate < 0) {
+		confirmDate = Number(PROMPT.question(`\nIs this alright? [1=yes,0=no]`));
+		if (isNaN(confirmDate) || (confirmDate === YES && confirmDate === NO)) {
 			console.log("INVALID INPUT");
 			PROMPT.question(`\nPress enter to continue.`)
 			process.stdout.write('\x1Bc');
 		}
 	}
-	confirmDate = Number(confirmDate);
 	if (confirmDate === NO) {
 		pickDue();
 	}
@@ -364,16 +358,15 @@ function printInfo() {
 function confirmVar(statement) {
 	let checkVar = -1;
 	process.stdout.write('\x1Bc');
-	while (isNaN(checkVar) || checkVar > 1 || checkVar < 0) {
+	while (isNaN(checkVar) || (checkVar !== YES && checkVar !== NO)) {
 		console.log(statement);
-		checkVar = PROMPT.question(`\nIs this correct? [1=yes,0=no]\n`);
-		if (isNaN(checkVar) || checkVar > 1 || checkVar < 0) {
+		checkVar = Number(PROMPT.question(`\nIs this correct? [1=yes,0=no]\n`));
+		if (isNaN(checkVar) || (checkVar !== YES && checkVar !== NO)) {
 			console.log("INVALID INPUT");
 			PROMPT.question(`\nPress enter to continue.`)
 			process.stdout.write('\x1Bc');
 		}
 	}
-	checkVar = Number(checkVar);
 	return checkVar;
 }
 
